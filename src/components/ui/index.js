5f1381b45eb2aa54b9b816f2ee934576403acdc4
Marcos <marcos5subero@gmail.com>
2026-03-12 19:45:23 +0100

    Create index.js

---
+++ src/components/ui/index.js
@@ -0,0 +1,10 @@
+export { default as Avatar } from "./Avatar";
+export { default as Badge } from "./Badge";
+export { default as Button } from "./Button";
+export { default as Card, CardBody, CardFooter, CardHeader } from "./Card";
+export { default as Chip } from "./Chip";
+export { default as EmptyState } from "./EmptyState";
+export { default as Input } from "./Input";
+export { default as Loader } from "./Loader";
+export { default as SectionHeader } from "./SectionHeader";
+export { default as Textarea } from "./Textarea";
