ec6cc6cc71a665e5486dd77d03c663b7bbe8f060
Tu Nombre <tuemail@ejemplo.com>
2026-03-12 17:10:22 +0100

    Connect frontend to deployed backend

--- src/config/apiBase.js
+++ src/config/apiBase.js
@@ -1,5 +1,4 @@
-const API_BASE = import.meta.env.VITE_API_BASE || "http://127.0.0.1:8000";
+const API_BASE =
+  import.meta.env.VITE_API_BASE || "https://app-backend-jd8f.onrender.com";
 
-export function getApiBase() {
-  return API_BASE;
-}
+export default API_BASE;
