507437d7b8a0ce1f3ba5126bdaf046b4c368e1b5
Marcos <marcos5subero@gmail.com>
2026-03-13 10:17:22 +0100

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -4,18 +4,16 @@ import { HashRouter } from "react-router-dom";
 import App from "./App.jsx";
 import { ToastProvider } from "./hooks/useToast.jsx";
 import { AuthProvider } from "./hooks/useAuth";
-
-import "./index.css";
 import "./styles/app.css";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <HashRouter>
-      <ToastProvider>
-        <AuthProvider>
+      <AuthProvider>
+        <ToastProvider>
           <App />
-        </AuthProvider>
-      </ToastProvider>
+        </ToastProvider>
+      </AuthProvider>
     </HashRouter>
-  </StrictMode>
+  </StrictMode>,
 );
