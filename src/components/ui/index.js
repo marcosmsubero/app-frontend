412c63b095d6aafed7d2ae092ad82e805923b3c5
Marcos <marcos5subero@gmail.com>
2026-03-13 12:52:25 +0100

    Update index.js

--- src/components/ui/index.js
+++ src/components/ui/index.js
@@ -1,7 +1,7 @@
 export { default as Avatar } from "./Avatar";
 export { default as Badge } from "./Badge";
 export { default as Button } from "./Button";
-export { default as Card, CardBody, CardFooter, CardHeader } from "./Card";
+export { default as Card, CardBody, CardFooter, CardHeader, CardTitle } from "./Card";
 export { default as Chip } from "./Chip";
 export { default as EmptyState } from "./EmptyState";
 export { default as Input } from "./Input";
