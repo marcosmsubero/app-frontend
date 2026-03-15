11e95f31390de9e16d0342459ad22cc72015b92c
Marcos <marcos5subero@gmail.com>
2026-03-15 17:36:19 +0100

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -5,7 +5,6 @@ import App from "./App.jsx";
 import { ToastProvider } from "./hooks/useToast.jsx";
 import { AuthProvider } from "./hooks/useAuth";
 import "./styles/app.css";
-import "./styles/settings-shell.css";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
