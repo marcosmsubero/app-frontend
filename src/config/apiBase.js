46065e85bdab48f37b9fcb1b05a116d62a94b276
Marcos <marcos5subero@gmail.com>
2026-03-31 16:00:42 +0200

    Update apiBase.js

--- src/config/apiBase.js
+++ src/config/apiBase.js
@@ -1,11 +1,2 @@
-const FALLBACK_API_BASE = "https://app-backend-jd8f.onrender.com";
-
-export const API_BASE = (import.meta.env.VITE_API_BASE || FALLBACK_API_BASE).replace(/\/+$/, "");
-
-export function buildApiUrl(path = "") {
-  if (!path) return API_BASE;
-  const normalizedPath = path.startsWith("/") ? path : `/${path}`;
-  return `${API_BASE}${normalizedPath}`;
-}
-
-export default API_BASE;
+export { API_BASE, buildApiUrl, buildEventStreamUrl } from "./api.js";
+export { default } from "./api.js";
