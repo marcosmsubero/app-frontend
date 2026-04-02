54512fd0b44f462af8241ab3578c801bd423a2bb
Marcos <marcos5subero@gmail.com>
2026-04-02 14:38:07 +0200

    Delete src directory

--- src/components/ui/index.js
+++
@@ -1,8 +0,0 @@
-export { default as Avatar } from "./Avatar";
-export { default as Badge } from "./Badge";
-export { default as Button } from "./Button";
-export { default as Card, CardBody, CardFooter, CardHeader, CardTitle } from "./Card";
-export { default as Chip } from "./Chip";
-export { default as EmptyState } from "./EmptyState";
-export { default as Input } from "./Input";
-export { default as Loader } from "./Loader";
