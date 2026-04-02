2ac84d5284d33624827271e0d6e4e083ca48f4d4
Marcos <marcos5subero@gmail.com>
2026-04-02 14:26:53 +0200

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,8 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   base: '/app-frontend/',
+  build: {
+    outDir: 'docs',
+    emptyOutDir: true,
+  },
 })
