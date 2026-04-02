dbe6d5e173a02da51cb674a5d9f7a8b55f32176f
Marcos <marcos5subero@gmail.com>
2026-04-02 13:21:04 +0200

    Update Chip.jsx

--- src/components/ui/Chip.jsx
+++ src/components/ui/Chip.jsx
@@ -1,34 +1,7 @@
-function joinClasses(...values) {
-  return values.filter(Boolean).join(" ");
-}
-
-export default function Chip({
-  children,
-  active = false,
-  size = "md",
-  className = "",
-  as: Component = "button",
-  type = "button",
-  ...props
-}) {
-  const classes = joinClasses(
-    "ui-chip",
-    `ui-chip--${size}`,
-    active && "ui-chip--active",
-    className
-  );
-
-  if (Component !== "button") {
-    return (
-      <Component className={classes} {...props}>
-        {children}
-      </Component>
-    );
-  }
-
+export default function Chip({ children, active }) {
   return (
-    <button type={type} className={classes} {...props}>
+    <div className={`ui-chip ${active ? "ui-chip--active" : ""}`}>
       {children}
-    </button>
+    </div>
   );
 }
