ca48e4be26002a64a1dca007c90e436211328071
Marcos <marcos5subero@gmail.com>
2026-04-03 13:12:28 +0200

    Update MobileShell.jsx

--- src/layouts/MobileShell.jsx
+++ src/layouts/MobileShell.jsx
@@ -1,7 +1,7 @@
 import { Outlet } from "react-router-dom";
 import BottomNav from "../components/BottomNav";
-import "../styles/mobile-shell.css";
 import logo from "../assets/Logo.png";
+import "../styles/mobile-shell.css";
 
 export default function MobileShell() {
   return (
@@ -12,11 +12,13 @@ export default function MobileShell() {
         </div>
       </header>
 
-      <main className="appChrome__frame">
-        <div className="appChrome__content">
-          <Outlet />
-        </div>
-      </main>
+      <div className="appChrome__frame">
+        <main className="appChrome__main">
+          <div className="appChrome__content">
+            <Outlet />
+          </div>
+        </main>
+      </div>
 
       <BottomNav />
     </div>
